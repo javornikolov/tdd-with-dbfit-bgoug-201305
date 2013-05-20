e8c87ee2dbe8906e802bb7f96e03182e92e1ba92
Yavor Nikolov <yavor.nikolov@cvidya.com>
2013-05-21 02:00:48 +0300

    Added sql*plus setup script for loading DDLs

---
+++ dbfitdemo/setup/setup_main.sql
@@ -0,0 +1,33 @@
+set echo on serveroutput on feedback on linesize 2000 pagesize 2000 trimspool on
+
+-- Database objects deployment
+
+@@ main/rdbms/oracle/bgoug/tables/tab1.tab
+
+@@ main/rdbms/oracle/bgoug/tables/testtbl.tab
+
+@@ main/rdbms/oracle/bgoug/tables/parameters.tab
+
+@@ main/rdbms/oracle/bgoug/triggers/trg_tab1_id.sql
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/time_machine.pks
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/time_machine.pkb
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/utl_params.pks
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/utl_params.pkb
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/zlpad_notrunc.pls
+show errors;
+
+@@ main/rdbms/oracle/bgoug/plsql/set_age.pls
+show errors;
+
+exit
+
