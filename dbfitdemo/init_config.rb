d1f08ea9ff0b2603265a2261b7d3061043108fbc
Yavor Nikolov <nikolov.javor@gmail.com>
2013-05-21 02:05:21 +0300

    init_config added (and config.rb.sample)

---
+++ dbfitdemo/init_config.rb
@@ -0,0 +1,16 @@
+require 'configatron_plus'
+
+def init()
+	init_custom_config()
+	ConfigatronPlus.fetch_sources
+end
+
+def init_custom_config()
+	parent_folder = File.expand_path( File.dirname( File.dirname( __FILE__ ) ) )
+	custom_config = File.join( parent_folder, 'config.rb' )
+
+	if File.file?( custom_config )
+		configatron.configatron_plus.source_files = { :current => custom_config }
+	end
+end
+
