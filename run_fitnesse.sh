c125117d2eabe53216ea9d601768dd764dc2d0b0
Yavor Nikolov <yavor.nikolov@cvidya.com>
2014-03-05 22:25:30 +0200

    Fitnesse/dbfit start update: use shell script Use startFitnesse.sh instead of direct java -jar when running dbfit

--- run_fitnesse.sh
+++ run_fitnesse.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cd "${FITNESSE_HOME}" && java -jar "${FITNESSE_HOME}/lib/fitnesse-standalone.jar" \
-	-p "${FITNESSE_PORT}" -e 0 -d ${PROJECT_ROOT}/src/test/rdbms/dbfit
-
+rake -f "${PROJECT_ROOT}/Rakefile" run_dbfit
