fe091e6881757bce0110635de8e97fbf3d7af1e2
Yavor Nikolov <yavor.nikolov@cvidya.com>
2014-03-05 23:05:31 +0200

    spec: migrate from shoud to expect syntax

--- dbfitdemo/spec/dbfit_bgoug_suite_spec.rb
+++ dbfitdemo/spec/dbfit_bgoug_suite_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe "DbfitBgougSuite" do
   let(:fitnesse_return) { run_dbfit_suite() }
-  it { fitnesse_return.exitstatus.should == 0 }
+  it { expect(fitnesse_return.exitstatus).to eq 0 }
 end
 
