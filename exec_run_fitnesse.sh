e3d758bbbd9d4e3142af218e94bd2684cbe0d526
Yavor Nikolov <nikolov.javor@gmail.com>
2014-03-05 22:25:30 +0200

    Fitnesse/dbfit start update: use shell script Use startFitnesse.sh instead of direct java -jar when running dbfit

--- exec_run_fitnesse.sh
+++ exec_run_fitnesse.sh
@@ -2,9 +2,6 @@
 
 PROJECT_NAME=dbfitdemo
 
-FITNESSE_HOME=/u01/app/dbfit
-FITNESSE_PORT=8981
-
 ROOT_DIR=`dirname ${0}`
 
 cd "${ROOT_DIR}" || { echo 'Cannot change to $ROOT_DIR'; exit 1; }
@@ -14,7 +11,7 @@ PROJECT_ROOT="${ROOT_DIR}/${PROJECT_NAME}"
 
 cd "${ROOT_DIR}"
 
-export PROJECT_ROOT FITNESSE_HOME FITNESSE_PORT
+export PROJECT_ROOT
 
 nohup ./run_fitnesse.sh >run_fitnesse.log 2>&1 &
 
